2b7fa93dae455bdf6a65a2ebdae22de29d0b1520
Edgar Hovhannisyan <edgar.hovhannisyan@eridatech.com>
2024-11-04 13:12:34 +0400

    Added env api

--- order_management_client/src/services/api.js
+++ order_management_client/src/services/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-const API_URL = 'http://172.16.10.75:5001';
+const {REACT_APP_API_URL} = process.env;
 
 export const getClientsAndOrders = async () => {
-    const response = await axios.get(`${API_URL}/orders`);
+    const response = await axios.get(`${REACT_APP_API_URL}/orders`);
     return response.data;
 };
 
 export const fetchClients = async () => {
-    const response = await axios.get(`${API_URL}/clients/all`);
+    const response = await axios.get(`${REACT_APP_API_URL}/clients/all`);
     return response.data;
 };
 
 export const addClient = async (clientData) => {
-    return await axios.post(`${API_URL}/clients`, clientData);
+    return await axios.post(`${REACT_APP_API_URL}/clients`, clientData);
 };
 
 export const addOrder = async (orderData) => {
-    return await axios.post(`${API_URL}/orders`, orderData);
+    return await axios.post(`${REACT_APP_API_URL}/orders`, orderData);
 };
